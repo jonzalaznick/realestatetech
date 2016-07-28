97c2ebba5ea8b16c496f23faf1b38bb5b2d58e34
jonzalaznick <jonzalaznick@gmail.com>
2016-07-28 11:46:55 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var main = function() {
+  	
+
+	$('#menu').hover(function() {
+		$('#menu .dropdown-menu').toggle();
+	});
+	
+	
+
+
+
+
+};
+
+
+
+
+$(document).ready(main);
+
+
+
+
+
+
+
+
+
+
+
+
